349af015b69c3fa6dc56107f7880d5bdd13fca0a
ahmedabdelaziz5 <ahmedabdelaziz6019@gmail.com>
2023-09-26 19:03:07 +0300

    2 concepts

---
+++ JS_isJustInTimeLanguage.js
@@ -0,0 +1,51 @@
+/*
+
+we can devide the language into 3 types:
+
+1. compiled language : ex c++ and c
+
+- the code is compiled before it is executed
+
+- create an execution file and wait the user to open it 
+
+- transformation process : source code -> compiler -> machine code ( create am execution file )-> program
+
+
+2. interpreted language : ex Python and old version of JS
+
+- there is no  execution file created 
+
+- transformation process : source code -> program
+
+- the program will be executed/translated line by line
+
+
+3. just in time language : ex  JS  and Java
+
+- the same compilation process in the compiled language 
+
+- but the execution file is created during the execution which means that there is no execution file created + the program will be excuted automatically 
+
+- the code is compiled during the execution
+
+_________________________________________________note that ______________________________________________________  
+
+
+JavaScript is both an interpreted and a just-in-time (JIT) compiled language. This means that JavaScript code is first interpreted line by line,
+but the JavaScript engine also uses a JIT compiler to compile the code to machine code as it is running. This allows JavaScript to be both fast and flexible.
+
+- Interpretation : When JavaScript code is interpreted, the JavaScript engine reads the code and executes it line by line. This is a relatively slow process,
+but it is also very flexible. It allows JavaScript to be used in a variety of environments, including web browsers and servers.
+
+- Just-in-time (JIT) compilation : JIT compilation is a process where the JavaScript engine compiles JavaScript code to machine code as it is running. 
+This makes JavaScript faster, but it also makes it less flexible.
+
+JavaScript engines : 
+
+JavaScript engines are responsible for interpreting and compiling JavaScript code. Some of the most popular JavaScript engines include:
+
+1- V8 (used in Chrome and Node.js)
+2- SpiderMonkey (used in Firefox)
+3- JavaScriptCore (used in Safari)
+
+*/
---
+++ garbage_collector.js
@@ -0,0 +1,37 @@
+/*
+
+- if we tried to open dev tools in the browser, we would see that the garbage collector is running every 1 second
+
+- without garbage collector, we would have to manually free up memory maually and this may cause memory leaks
+
+- example on memory leak in javascript -> try to open dev tools and to type this function in the console : 
+
+*/
+
+
+function memoryLeak() {
+    this.variable = 'variable';
+}
+
+// - then try to invoke the function by calling it in the console : 
+
+function memoryLeak() {
+    this.variable = 'variable';
+}
+
+memoryLeak();
+
+/*
+
+- after invoking the function try typing ${window} in the console -> this will return the window object
+
+- open the window obj you will see that the variable is there and it is not deleted from the memory after the function is invoked and this is a memory leak
+and this is because uneffient usage of memory in javascript
+
+- to solve this problem we need to delete the variable from the memory after the function is invoked
+
+- garbage collector is a process that runs in the background and it is responsible for freeing up memory that is no longer used so that the memory can be reused
+and no memeory leaks will happen
+
+*/
+
