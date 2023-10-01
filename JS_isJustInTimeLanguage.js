4247e43d456ad70b912f279df9bff597d30ae9a0
ahmedabdelaziz5 <ahmedabdelaziz6019@gmail.com>
2023-10-01 17:28:10 +0300

    2nd concept

--- JS_isJustInTimeLanguage.js
+++ JS_isJustInTimeLanguage.js
@@ -1,8 +1,21 @@
 /*
 
-we can devide the language into 3 types:
 
-1. compiled language : ex c++ and c
+                                                we will go throuth the following points :
+|------------------------------------------------------------------------------------------------------------|
+| 1- types of programming languages and what is the difference between them ?
+| 2- what kind of languages is JS ?
+|------------------------------------------------------------------------------------------------------------|
+
+
+______________________________________________________________________________________________________________________________________________________First Part : 
+
+
+** types of programming languages and what is the difference between them ?
+
+we can devide the language into 3 types :
+
+1- compiled language : (like : c++ and c)
 
 - the code is compiled before it is executed
 
@@ -11,16 +24,16 @@ we can devide the language into 3 types:
 - transformation process : source code -> compiler -> machine code ( create am execution file )-> program
 
 
-2. interpreted language : ex Python and old version of JS
+2- interpreted language : (like : Python and old version of JS)
 
-- there is no  execution file created 
+- there is no execution file created 
 
 - transformation process : source code -> program
 
 - the program will be executed/translated line by line
 
 
-3. just in time language : ex  JS  and Java
+3- just in time language : (like :  JS  and Java)
 
 - the same compilation process in the compiled language 
 
@@ -28,11 +41,17 @@ we can devide the language into 3 types:
 
 - the code is compiled during the execution
 
-_________________________________________________ note that ______________________________________________________  
 
+______________________________________________________________________________________________________________________________________________________Second Part : 
 
-JavaScript is both an interpreted and a just-in-time (JIT) compiled language. This means that JavaScript code is first interpreted line by line,
-but the JavaScript engine also uses a JIT compiler to compile the code to machine code as it is running. This allows JavaScript to be both fast and flexible.
+** what kind of languages is JS ?
+
+- JavaScript is both an interpreted and a just-in-time (JIT) compiled language.
+
+- This means that JavaScript code is first interpreted line by line, but the JavaScript engine also uses a JIT compiler to compile the code to machine code as it is running. 
+-This allows JavaScript to be both fast and flexible.
+
+let's take a look on the behavior of JS in each aspect :
 
 - Interpretation : When JavaScript code is interpreted, the JavaScript engine reads the code and executes it line by line. This is a relatively slow process,
 but it is also very flexible. It allows JavaScript to be used in a variety of environments, including web browsers and servers.
@@ -40,13 +59,16 @@ but it is also very flexible. It allows JavaScript to be used in a variety of en
 - Just-in-time (JIT) compilation : JIT compilation is a process where the JavaScript engine compiles JavaScript code to machine code as it is running. 
 This makes JavaScript faster, but it also makes it less flexible.
 
-JavaScript engines : 
 
-JavaScript engines are responsible for interpreting and compiling JavaScript code. Some of the most popular JavaScript engines include:
+
+most common JavaScript engines : 
+
+JavaScript engines are responsible for interpreting and compiling JavaScript code. Some of the most popular JavaScript engines include :
 
 1- V8 (used in Chrome and Node.js)
 2- SpiderMonkey (used in Firefox)
 3- JavaScriptCore (used in Safari)
+
 
 IF THIS EXPLANATION WAS HELPFUL PLEASE GIVE ME A STAR :)
 
